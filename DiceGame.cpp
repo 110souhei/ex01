fb44207ccc4b12557d33ed535aba1c377a332ab1
110souhei <souhei103@gmail.com>
2023-06-09 11:13:30 +0900

    Dice game: initial revision

---
+++ DiceGame.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0; i< (n); i++)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 998244353;
+const int inf = (1<<30);
+ 
+int main(){
+	cout<<"Rolling dice.."<<endl;
+	int a = rand()%6+1;
+	int b = rand()%6+1;
+	cout<<"Die 1: "<<a<<endl;
+	cout<<"Die 2: "<<b<<endl;
+	cout<<"Total value: "<<a+b<<endl;
+}
+
