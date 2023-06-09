f7e3a5cbbd6b0beced2931c9fdfd6133c1618c29
110souhei <souhei103@gmail.com>
2023-06-09 11:29:54 +0900

    Greetings added

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -7,6 +7,9 @@ const int mod = 998244353;
 const int inf = (1<<30);
  
 int main(){
+	cout<<"What is your name?"<<endl;
+	string name; cin>>name;
+	cout<<"Hello, "<<name<<"!"<<endl;
 	cout<<"Rolling dice.."<<endl;
 	int a = rand()%6+1;
 	int b = rand()%6+1;
